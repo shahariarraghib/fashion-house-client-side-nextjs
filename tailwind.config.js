56dee11555b547d7f5b8fdd1dff44d5f0f208bde
shahariarraghib <sr.raghib@gmail.com>
2023-12-17 18:23:54 +0600

    tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,6 +5,8 @@ module.exports = {
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
-  
+  theme: {
+    extend: {},
+  },
   plugins: [require("daisyui")],
 };
