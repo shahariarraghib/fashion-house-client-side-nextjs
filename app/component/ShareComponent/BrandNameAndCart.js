ad04c8e8755e3a1b4629c1ce20b760f93fa4c887
shahariarraghib <sr.raghib@gmail.com>
2023-12-28 20:25:23 +0600

    add css

--- app/component/ShareComponent/BrandNameAndCart.js
+++ app/component/ShareComponent/BrandNameAndCart.js
@@ -57,7 +57,7 @@ const BrandNameAndCart = () => {
 
   return (
     <div>
-      <nav className="">
+      <nav>
         <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
           <div className="flex items-center justify-between h-16">
             <div className="flex-shrink-0">
